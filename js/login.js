e70670a8476b1783d953ae36f3157fc0e39830ad
Khademul Islam Apon <khademulislamapon10@gmail.com>
2024-09-19 23:48:25 +0600

    maked folder for work with desgin lowgin page and do some js

---
+++ js/login.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-login').addEventListener('click', function (event) {
+    event.preventDefault;
+
+    const mobileNumber = document.getElementById('mobile-number').value;
+    console.log('Number:', mobileNumber);
+
+    /* const pinNum = document.getElementById('pin-num').value;
+    console.log('Pin Number:', pinNum); */
+});
+
