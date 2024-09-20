9c21ad2c0a1fe035cc66f78999d50c0e0cc4aa9b
Khademul Islam Apon <khademulislamapon10@gmail.com>
2024-09-20 09:54:14 +0600

    compleate add money section dome

--- js/home.js
+++ js/home.js
@@ -1,10 +1,20 @@
 document.getElementById('btn2-add-money').addEventListener('click', function(event) {
     event.preventDefault();
-    
+
+    const mainBalance = document.getElementById('main-balance').innerText;
+    // console.log(typeof mainBalance)
     const addMoneyAmount = document.getElementById('add-money-amount').value;
+    // console.log(typeof addMoneyAmount)
     const myPassword = document.getElementById('password').value;
+
     if(myPassword === '1230'){
-        balance = balance + addMoneyAmount;
+        const newBalance = Number(mainBalance) + Number(addMoneyAmount);
+        // mainBalance = parseFloat(mainBalance) + parseFloat(addMoneyAmount);
+        // mainBalance = parseInt(mainBalance) + parseInt(addMoneyAmount);
+        
+        // console.log(newBalance)
+
+        document.getElementById('main-balance').innerText = newBalance;
     }else{
         alert('information wrong')
     }
